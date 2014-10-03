18ab1a4e6f9fc3b3b5719d284c52ebb7d73882f9
rrgayhart <rrgayhart@gmail.com>
2014-10-03 14:12:39 -0600

    First pass refactor

--- user_commits.rb
+++ user_commits.rb
@@ -2,16 +2,23 @@ require 'faraday'
 require 'json'
 
 class UserCommits
-  attr_reader :username, :event_history, :messages
 
   def initialize(username)
     @username = username
-    @event_history = get_event_array
-    @messages = give_me_messages
   end
 
-  def give_me_messages
-    pull_commits.map do |commit|
+  def fetch_commits(event_history=nil)
+    event_history ||= get_event_history
+    push_event_data = pull_push_events(event_history)
+    payload_data = pull_payload(push_event_data)
+    commit_history = pull_commit_data(payload_data)
+    give_me_messages(commit_history)
+  end
+
+  private
+
+  def give_me_messages(commit_history)
+    commit_history.map do |commit|
       commit['message']
     end
   end
@@ -20,31 +27,30 @@ class UserCommits
     "https://api.github.com/users/#{username}/events"
   end
 
-  def get_event_array
+  def get_event_history
     response = Faraday.get(event_link)
     JSON.parse(response.body)
   end
 
-  def pull_push_events
+  def pull_push_events(event_history)
     event_history.select do |event|
       event["type"] == 'PushEvent'
     end
   end
 
-  def pull_payload
-    pull_push_events.collect do |push_event|
+  def pull_payload(push_events)
+    push_events.collect do |push_event|
       push_event['payload']
     end
   end
 
-  def pull_commits
-    commits = []
-    pull_payload.each do |payload|
-      payload['commits'].each do |commit|
-        commits.push(commit)
-      end
-    end
-    commits
+  def pull_commit_data(pull_payload_data)
+    pull_payload_data.collect do |payload|
+      payload['commits'].collect { |commit| commit }
+    end.flatten
   end
 
+  def username
+    @username.strip
+  end
 end
